1306ed7611b9ebff985528c9914ab9564b80e759
Matt Sprague <me@spragala.com>
2023-11-06 10:33:41 -0800

    chore: creates components to test state, nuxt components, etc

---
+++ components/base/Navigation/index.vue
@@ -0,0 +1,7 @@
+<template lang="pug" src="./index.pug"></template>
+
+<script setup>
+const { data } = await useAsyncData('navigation', () => getData('global', 'globalData'))
+
+const props = data.value
+</script>
---
+++ components/custom/HomeHero/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug" src="./index.pug"></template>
+
+<script setup>
+const { data } = defineProps({
+  data: Object
+})
+const activeText = ref(0)
+
+onMounted(()=> {
+  setInterval(() => {
+    activeText.value = (activeText.value + 1) % data.title.changing_text.length
+  }, 3000)
+})
+const iloaded = () => {
+  console.log('I loaded')
+}
+</script>
+
+<style lang="sass" src="./index.sass"></style>
