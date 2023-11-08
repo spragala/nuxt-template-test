64d8b8a03a982c752c1d45427ec5eb5e288f4624
Matt Sprague <me@spragala.com>
2023-11-08 10:18:25 -0800

    updates(app): fetches global data and passes to pages

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template lang="pug">
 main#main-content
   //- BaseNavigation
-  NuxtPage
+  NuxtPage(:globalData='globalData')
 </template>
+
+<script setup>
+const { data: globalData } = await usePostData('global', 'globalData')
+</script>
